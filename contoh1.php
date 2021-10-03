6825c6555f8732d245b981173c4b7f9459b0fcdb
ahmadhidayah1 <ahmaddayah99@gmail.com>
2021-10-03 15:35:38 +0700

    menambahkan contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1";
+        echo"Nama saya ahmad hidayah
+            saya tinggal di daerah cakung
+            olahraga yang saya sukai lari";
+    }
+}
